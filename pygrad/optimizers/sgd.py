b66b1d3b86c09d99faa439871088a93b221fa19a
George573 <george.garbaly@gmail.com>
2026-09-06 23:14:22 +0300

    Fixed spelling mistakes in SGD

--- pygrad/optimizers/sgd.py
+++ pygrad/optimizers/sgd.py
@@ -1,32 +1,29 @@
 class SGD:
-    def __init__(self, trainble_params, epcilon=1e-3, momentum=0.0):
-        if epcilon <= 0:
-            raise ValueError('epcilon must be positive')
-        
+    def __init__(self, trainable_params, epsilon=1e-3, momentum=0.0):
+        if epsilon <= 0:
+            raise ValueError("epsilon must be positive")
+
         if not 0 <= momentum < 1:
             raise ValueError("momentum must be in the range [0, 1]")
-        
-        self.traible_params = trainble_params
-        self.epcilon = epcilon
+
+        self.trainable_params = trainable_params
+        self.epsilon = epsilon
         self.momentum = momentum
         self.velocity_table = {}
 
     def step(self):
-        for param in self.traible_params:
+        for param in self.trainable_params:
             grad = getattr(param, "grad", None)
             if grad is None:
-                raise ValueError(f"Parametr {param} doesn't have a computed gradient")
-            
+                raise ValueError(f"Parameter {param} doesn't have a computed gradient")
+
             prev_v = self.velocity_table.get(param, 0)
-            
-            v = (
-                self.momentum * prev_v 
-                - self.epcilon * grad
-            )
-            
+
+            v = self.momentum * prev_v - self.epsilon * grad
+
             self.velocity_table[param] = v
             param.data += v
-            
+
     def zero_grad(self):
-        for param in self.traible_params:
+        for param in self.trainable_params:
             param.grad = None
